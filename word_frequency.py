faa253dd29aa58cb8fce902ecbff27a5e7c36226
Jolivet <mjolivet@parkrowmedia.com>
2020-06-26 20:58:40 +0000

    clean text function

--- word_frequency.py
+++ word_frequency.py
@@ -1,32 +1,37 @@
+import re
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
-
-
+extra =[]
+wordfreq = []
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    # Your code will go here. You can write additional functions if you want, and call them inside this function.
-    # first open the file
-    with open(file) as f:
-        lyrics = f.readlines()
-        word_list = [line.split() for line in lyrics]
-        print(word_list)
-
-# This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
+  
+  with open(file) as f:
+    for items in f:
+      extra.append(items)
+     
+   
+for items in extra:
+    clean = re.sub(r"[!?.,]","",items.lower())
+    cleaner = clean.split()
+    wordfreq.append(cleaner)
+    
 if __name__ == "__main__":
+
     import argparse
     from pathlib import Path
-
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
+        
+    parser = argparse.ArgumentParser (
+    description='Get the word frequency in a text file.')
     parser.add_argument('file', help='file to read')
     args = parser.parse_args()
 
-    file = Path(args.file)
-    if file.is_file():
+file = Path(args.file)
+if file.is_file():
         print_word_freq(file)
-    else:
+else:
         print(f"{file} does not exist!")
         exit(1)
